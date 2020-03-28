e508174294308823317d0a4a0df3f9243c7461dc
전해성 <seong0428@gmail.com>
2020-03-28 19:49:02 +0900

    Rollback index.ts

--- emergency-cloud-functions/functions/src/index.ts
+++ emergency-cloud-functions/functions/src/index.ts
@@ -1,27 +1,8 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-admin.initializeApp();
+import * as functions from "firebase-functions";
 
-export const publishNews = functions.firestore
-  .document('news/{newsId}')
-  .onCreate(async snapshot => {
-    const news = snapshot.data();
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-    const payload: admin.messaging.MessagingPayload = {
-      notification: {
-        title: 'News',
-        body: `${news.name} is ready for adoption`,
-        icon: 'your-icon-url',
-        click_action: 'FLUTTER_NOTIFICATION_CLICK' // required only for onResume or onLaunch callbacks
-      }
-    };
-
-    return fcm.sendToTopic(`${news.name}`, payload);
-  });
-
-exports.scheduledFunction = functions.pubsub
-  .schedule('every 30 minutes')
-  .onRun((context) => {
-    console.log('This will be run every 30 minutes!');
-    return null;
-  });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
