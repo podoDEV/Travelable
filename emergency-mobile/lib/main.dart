d2f5ec323619f27b373cef18f22f6d04adc9fc4d
Duckuism <rnejrl123456@gmail.com>
2020-04-11 11:25:20 +0900

    [WIP] main page

--- emergency-mobile/lib/main.dart
+++ emergency-mobile/lib/main.dart
@@ -14,44 +14,81 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  List<Country> _countries = Country.getCountries();
+  List<DropdownMenuItem<Country>> _dropdownMenuItems;
+  Country _selectedCountry;
+
+  void initState() {
+    _dropdownMenuItems = buildDropdownMenuItems(_countries);
+    _selectedCountry = _dropdownMenuItems[0].value;
+  }
+
+  List<DropdownMenuItem<Country>> buildDropdownMenuItems(List countries) {
+    List<DropdownMenuItem<Country>> items = List();
+    for (Country country in countries) {
+      items.add(DropdownMenuItem(
+        value: country,
+        child: Text(country.name),
+      ));
+    }
+    return items;
+  }
+
+  onChangeDropdownItem(Country selectedCountry){
+    setState((){
+      _selectedCountry = selectedCountry;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+              appBar: PreferredSize(
+                  preferredSize: Size.fromHeight(74.0),
+                  child: AppBar(
+                    flexibleSpace: SafeArea(
+                        child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        new TabBar(
+                            tabs: choices.map((Choice choice) {
+                          return Tab(
+                              text: choice.title, icon: Icon(choice.icon));
+                        }).toList())
+                      ],
+                    )),
+                  )),
+              body: new Container(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 20.0,),
+                      DropdownButton(
+                        value: _selectedCountry, 
+                        items: _dropdownMenuItems, 
+                        onChanged: onChangeDropdownItem,
+                      ),
+                      SizedBox(height: 20.0,),
+                    ],
+                  )
+                )
+              )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Choice {
+  const Choice({this.title, this.icon});
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  final IconData icon;
 }
 
 class _MyHomePageState extends State<MyHomePage> {
